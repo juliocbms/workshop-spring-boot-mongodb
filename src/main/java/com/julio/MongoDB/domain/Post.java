4edc7612091b0eb7c06ea7bbf2af8dcab7e7314d
Julio Cesar <juliocesar13b@gmail.com>
2025-08-09 17:38:36 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/julio/MongoDB/domain/Post.java
+++ src/main/java/com/julio/MongoDB/domain/Post.java
@@ -1,5 +1,6 @@
 package com.julio.MongoDB.domain;
 
+import com.julio.MongoDB.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -15,13 +16,13 @@ public class Post {
     private Date date;
     private String title;
     private String body;
-    private User author;
+    private AuthorDTO author;
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
@@ -29,7 +30,7 @@ public class Post {
 
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         this.id = id;
         this.date = date;
         this.title = title;
