596346f28dcfac464ef5cb2f075237da1fe4e443
Julio Cesar <juliocesar13b@gmail.com>
2025-08-09 14:28:54 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/julio/MongoDB/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.julio.MongoDB.repository;
+
+import com.julio.MongoDB.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User,String> {
+}
---
+++ src/main/java/com/julio/MongoDB/services/UserService.java
@@ -0,0 +1,19 @@
+package com.julio.MongoDB.services;
+
+import com.julio.MongoDB.domain.User;
+import com.julio.MongoDB.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+}
