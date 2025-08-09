5ee1323ae184f9fb229216344c6b7442badd3b3e
Julio Cesar <juliocesar13b@gmail.com>
2025-08-09 17:50:18 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/julio/MongoDB/resources/UserResource.java
+++ src/main/java/com/julio/MongoDB/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.julio.MongoDB.resources;
 
+import com.julio.MongoDB.domain.Post;
 import com.julio.MongoDB.domain.User;
 import com.julio.MongoDB.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,5 +55,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
